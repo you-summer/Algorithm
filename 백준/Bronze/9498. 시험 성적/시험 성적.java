ecbfbe274526e60194b558c58899ccf792209b6d
you-summer <151509975+you-summer@users.noreply.github.com>
2024-02-04 23:02:11 +0900

    [Bronze V] Title: 시험 성적, Time: 212 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main { 
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+
+		
+		if(100>=A&&A>=90) {
+			System.out.println("A");
+		}else if(89>=A&&A>=80){
+			System.out.println("B");
+		}else if(79>=A&&A>=70){
+			System.out.println("C");
+		}else if(69>=A&&A>=60) {
+			System.out.println("D");
+		}else {
+			System.out.println("F");
+		}
+		
+
+	}
+
+}
